dc3017ad582f58f94042a7479752084a6f1ee477
tarunvyas-maglabs <tarunvyas@maglabs.in>
2026-03-26 19:59:45 +0530

    Eslint added

--- mongo.js
+++ mongo.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3){
-    console.log('give password as an argument');
-    process.exit(1);
+  console.log('give password as an argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url = `mongodb+srv://tarunvyas16_db_user:${password}@cluster0.sygq5dz.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url, {family: 4})
+mongoose.connect(url, { family: 4 })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean
+  content: String,
+  important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -33,9 +33,9 @@ note.save().then(result => {
 */
 
 
-Note.find({important : true}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
+Note.find({ important : true }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
